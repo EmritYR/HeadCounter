bb7f41c3de4a2b44bf38bf6e061a2ca7ba4ae626
Bhavesh117 <bhavesh.digamber@my.uwi.edu>
2019-11-24 16:57:33 -0400

    updated server connection

--- server.js
+++ server.js
@@ -6,24 +6,22 @@ const path = require("path");
 const app = express();
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log("App listening on port: " + port)
 /*** End Hosting server for heroku  ***/
 
 const { Client } = require("pg");
-const connectionString =
-  "postgres://qqolorykjuhkzg:aaf3efea7997f8b655d1b34dcffd6c3c5664eafdc4fb58591adef8df6b780a15@ec2-54-221-195-148.compute-1.amazonaws.com:5432/d3bfq4clh09b21?ssl=true";
-
 const client = new Client({
-  connectionString: connectionString
+  connectionString: 'postgres://qqolorykjuhkzg:aaf3efea7997f8b655d1b34dcffd6c3c5664eafdc4fb58591adef8df6b780a15@ec2-54-221-195-148.compute-1.amazonaws.com:5432/d3bfq4clh09b21',
+  ssl: true,
 });
 
 client.connect();
 
 
 app.get("/account", function(req, res) {
-  client.query("SELECT * FROM lecturer", [1], function(err, result) {
+  client.query("SELECT * FROM lecturer", function(err, result) {
     if (err) {
       console.log(err);
       res.status(400).send(err);
