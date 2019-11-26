25288c390429993151a3caa06e358f7d6b7c8986
Bhavesh117 <bhavesh.digamber@my.uwi.edu>
2019-11-26 17:55:42 -0400

    added conf to server js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const SqlString = require("sqlstring");
 const { Client } = require("pg");
-const config = require("./src/config/database");
+const config = {
+  database: 'postgres://qqolorykjuhkzg:aaf3efea7997f8b655d1b34dcffd6c3c5664eafdc4fb58591adef8df6b780a15@ec2-54-221-195-148.compute-1.amazonaws.com:5432/d3bfq4clh09b21',
+  secret: 'supersecret'
+};
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 
