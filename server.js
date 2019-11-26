1b5733f155876b24501be6318acab02802b4fcc2
Bhavesh117 <bhavesh.digamber@my.uwi.edu>
2019-11-26 18:03:17 -0400

    added conf to server js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const client = new Client({
   connectionString: config.database,
